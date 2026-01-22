fe7ac8fd7222fe9b9379d94879f7860d36631a83
Dayron Mustelier <dayrondotres@gmail.com>
2026-01-22 03:42:50 +0100

    feat: add run.bat for launching the Python application

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+:: This is for not showing the command of the batch file
+
+:: start → Open a new window to run a specified program or command
+:: cmd /k → Carries out the command specified by string but remains
+:: ^&^ → allows the command to continue on multiple lines (more readable)
+:: cd /d "%~dp0" → Change directory to the location of this batch file
+
+start "My Python Application" cmd /k ^
+    cd /d "%~dp0" ^&^
+    python src/app.py ^&^
+    echo. ^&^
+    echo Press any key to exit... ^&^
+    pause
