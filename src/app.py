7e5c7ef22e85fa29b154bba9dcfbc126c6b499b2
Dayron Mustelier <dayrondotres@gmail.com>
2026-01-23 04:58:26 +0100

    refactor: move media utilities to a separate module

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from settings.manager import SettingsManager
 
 from utils import clear_console, clear_screen, get_source_via_dialog
 from cli.styles import R, Y
-from media import frame_to_text, frames_to_images, images_to_video
+from media.media import frame_to_text, frames_to_images, images_to_video
 
 # ============================================================
 #                     MAIN APPLICATION
